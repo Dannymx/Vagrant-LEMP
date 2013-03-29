a32f12f9305fd11f77ea1bd68e62be583805ae79
Deuzu <deuzubis@gmail.com>
2013-03-29 01:58:42 +0100

    1st commit

--- lamp.sh
+++ lamp.sh
@@ -1,38 +1,50 @@
-aptitude update && aptitude safe-upgrade
-
-# MANDATORY
-aptitude install -y vim
-
-
-# LAMP
-
 # apache2
+echo "#########################"
+echo "#  INSTALLING APACHE2   #"
+echo "#########################"
 aptitude install -y apache2
 a2enmod rewrite
 
 # mysql
+echo ""
+echo "#########################"
+echo "#   INSTALLING MYSQL    #"
+echo "#########################"
 aptitude install -y mysql-server
 mysql_secure_installation
 sed -i 's/[mysqld]\n/[mysqld]\ncollation-server = utf8_general_ci\ncharacter-set-server = utf8/' /etc/mysql/my.cnf
 
 # php5
+echo ""
+echo "#########################"
+echo "#    INSTALLING PHP5    #"
+echo "#########################"
 aptitude install -y php5
 
 # composer
-aptitude install -y curl
+echo ""
+echo "#########################"
+echo "#  INSTALLING COMPOSER  #"
+echo "#########################"
+dpkg -s curl >/dev/null 2>&1 && {
+	echo "=> Curl allready installed skipping to composer install."
+} || {
+	echo "=> Curl currently not installed. Doing it."
+	aptitude install -y curl
+}
 echo "suhosin.executor.include.whitelist = phar" >> /etc/php5/cli/php.init
 curl -s https://getcomposer.org/installer | php
 
 # git
+echo ""
+echo "#########################"
+echo "#    INSTALLING GIT     #"
+echo "#########################"
 aptitude install -y git
 
 # restarting apache2
+echo ""
+echo "#########################"
+echo "#  RESTARTING APACHE2   #"
+echo "#########################"
 /etc/init.id/apache2 restart
-
-
-# SECURITY
-
-# Reconfigure sshd - change port and disable root login
-sed -i 's/^Port [0-9]*/Port '${SSHD_PORT}'/' /etc/ssh/sshd_config
-sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-service ssh reload
