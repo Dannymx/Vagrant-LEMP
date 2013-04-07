8b110b8d23b6808f7616a2587f1d5615101ec095
Deuzu <deuzubis@gmail.com>
2013-04-07 20:06:56 +0200

    Readd php5-mysql in mysql.sh

--- databases/mysql.sh
+++ databases/mysql.sh
@@ -7,7 +7,7 @@ echo "#   INSTALLING MYSQL    #"
 echo "#########################"
 sudo debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password password toor'
 sudo debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password_again password toor'
-sudo aptitude install -y mysql-server
+sudo aptitude install -y mysql-server php5-mysql
 # mysql_secure_installation
 # sed -i "s/[mysqld](\n|\r\n)/[mysqld]\ncollation-server = utf8_general_ci\ncharacter-set-server = utf8\nskip-character-set-client-handshake" /etc/mysql/my.cnf
 # [mysqld]
