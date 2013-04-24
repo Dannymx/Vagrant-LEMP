1de2138f365d58c1d3cac35bedff19034986ba9c
Deuzu <deuzubis@gmail.com>
2013-04-24 12:13:22 +0300

    Create install.sh

---
+++ symfony/install.sh
@@ -0,0 +1,7 @@
+# Symfony2 alias 
+echo ""
+echo "#########################"
+echo "#       SF2 ALIAS       #"
+echo "#########################"
+echo "sf2 = 'php app/console'" >> /home/vagrant/.bashrc
+echo "sf2clear = 'php app/console cache:clear'" >> /home/vagrant/.bashrc
