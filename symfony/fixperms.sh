2eaa92d8ed60ba77d6ccd270ef545a2b9acafdfd
Florian Touya <deuzubis@gmail.com>
2014-01-30 14:55:46 +0100

    Update fixperm.sh to add more options

--- symfony/fixperms.sh
+++ symfony/fixperms.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 
+if [ $# -gt 0 ]; then
+  argList=(--set-all --set-user --set-httpd)
+  match=0
+  for arg in "${argList[@]}"; do
+    if [[ $1 = $arg ]];
+    then
+      match=1
+      break
+    fi
+  done
+
+  if [[ $match = 0 ]]; then
+    echo "Invalid argument supplied. Chose one of the following : "
+    
+    for arg in "${argList[@]}"; do
+      echo "    $arg"
+    done
+
+    exit
+  fi
+fi
 
 echo ""
 echo "#########################"
 echo "# PERMISSIONS"
 echo "#########################"
-read -p "# User who can write the DIR ? " user
-read -p "# Httpd user ? " httpd
+
+user=$(whoami)
+httpd=$(ps axho user,comm|grep -E "httpd|apache"|uniq|grep -v "root"|awk 'END {if ($1) print $1}')
+
+if [ $# -gt 0 ]; then
+  if [[ $1 = "--set-all" ]]; then
+    read -p "# User who can write the DIR ? " user
+    read -p "# Httpd user ? " httpd
+  else
+    if [[ $1 = "--set-user" ]]; then
+      read -p "# User who can write the DIR ? " user
+    elif [[ $1 = "--set-httpd" ]]; then
+      read -p "# Httpd user ? " httpd
+    fi
+  fi
+fi
+
 echo "# Setting permissions"
-chown -R $user:$user .
-chown -R $user:$httpd app/cache app/logs
-chmod -R u+w .
-chmod -R g+w app/cache app/logs
+#chown -R $user:$user .
+#chown -R $user:$httpd app/cache app/logs
+#chmod -R u+w .
+#chmod -R g+w app/cache app/logs
+echo "# Permissions set for $user and $httpd."
