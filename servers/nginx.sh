fb8846818d573269c069f6c45ace323588fc3893
Dan <eldan89@gmail.com>
2014-03-04 04:15:03 -0600

    Create Nginx Install script

--- servers/nginx.sh
+++ servers/nginx.sh
@@ -1,1 +1,6 @@
-#TODO install nginx
+#Install Nginx (Nginx-extras)
+echo ""
+echo "#########################"
+echo "#  INSTALLING APACHE2   #"
+echo "#########################"
+sudo apt-get install -y nginx-extras
