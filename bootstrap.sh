9a42d316c5fd04ae66863cc2c655905a3f9fb045
Dan <eldan89@gmail.com>
2014-03-04 04:08:02 -0600

    Customize to needs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,63 +3,44 @@
 echo "#########################"
 echo "# ADDING DOTDEB SOURCES #"
 echo "#########################"
-echo "deb http://packages.dotdeb.org squeeze all" >> /etc/apt/sources.list
-echo "deb-src http://packages.dotdeb.org squeeze all" >> /etc/apt/sources.list
-echo "deb http://packages.dotdeb.org squeeze-php54 all" >> /etc/apt/sources.list
-echo "deb-src http://packages.dotdeb.org squeeze-php54 all" >> /etc/apt/sources.list
-cd /home
+echo "deb http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
+echo "deb-src http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
+echo "deb http://packages.dotdeb.org wheezy-php54 all" >> /etc/apt/sources.list
+echo "deb-src http://packages.dotdeb.org wheezy-php54 all" >> /etc/apt/sources.list
 wget http://www.dotdeb.org/dotdeb.gpg
 cat dotdeb.gpg | sudo apt-key add -
 rm dotdeb.gpg
 
 echo ""
-echo "#################################"
-echo "#  UPGRADING APTITUDE & STUFF   #"
-echo "#################################"
-sudo echo grub-common hold | dpkg --set-selections
-sudo echo grub-pc hold | dpkg --set-selections
-sudo apt-get update --fix-missing
-sudo aptitude safe-upgrade -y
-sudo aptitude install -y nfs-common
+echo "###########################"
+echo "#    UPDATING PACKAGES    #"
+echo "###########################"
+sudo apt-get update 
+
+echo ""
+echo "###########################"
+echo "#     INSTALLING STUFF    #"
+echo "###########################"
+sudo apt-get install -y htop curl wget git
 
 # php5
 echo ""
 echo "#########################"
 echo "#    INSTALLING PHP5    #"
 echo "#########################"
-sudo aptitude install -y php5 php5-fpm php5-curl php5-gd php5-cli php5-apc php5-intl
+sudo apt-get install -y php5 php5-fpm php5-curl php5-gd php5-cli php5-apc php5-intl php5-mcrypt
 
 # composer
 echo ""
 echo "#########################"
 echo "#  INSTALLING COMPOSER  #"
 echo "#########################"
-echo "suhosin.executor.include.whitelist = phar" >> /etc/php5/cli/php.ini
-sudo aptitude install -y curl
 if [ -f /usr/bin/composer ]; then
     echo "=> Composer already installed skipping to git."
 else
-    cd /home
     curl -s https://getcomposer.org/installer | php
     mv composer.phar /usr/bin/composer
 fi
-
-# git
-echo ""
-echo "#########################"
-echo "#    INSTALLING GIT     #"
-echo "#########################"
-sudo aptitude install -y git
-
-# add vim
-echo ""
-echo "#########################"
-echo "#      VIM CUSTOM       #"
-echo "#########################"
-sudo aptitude install -y vim
-echo "syntax on" >> /home/vagrant/.vimrc
-echo "set number" >> /home/vagrant/.vimrc
-sudo cp /home/vagrant/.vimrc /root/ && sudo chown root:root /root/.vimrc
 
 # add alias
 echo ""
@@ -72,21 +53,6 @@ echo "alias ...='cd ../../'" >> /home/vagrant/.bashrc
 echo "alias ....='cd ../../../'" >> /home/vagrant/.bashrc
 echo "alias .....='cd ../../../../'" >> /home/vagrant/.bashrc
 echo "alias ......='cd ../../../../../'" >> /home/vagrant/.bashrc
-echo "alias a2r='/etc/init.d/apache2 restart'" >> /home/vagrant/.bashrc 
-echo "alias a2l='tail -f /var/log/apache2/error.log'" >> /home/vagrant/.bashrc
-
-# Git alias 
-echo ""
-echo "#########################"
-echo "#       GIT ALIAS       #"
-echo "#########################"
-echo "alias ga='git add .'" >> /home/vagrant/.bashrc
-echo "alias gca='git commit -a -m'" >> /home/vagrant/.bashrc
-echo "alias gph='git push'" >> /home/vagrant/.bashrc
-echo "alias gcl='git pull'" >> /home/vagrant/.bashrc
-
-sudo echo "<?php phpinfo(); ?>" >> /var/www/index.php 
-sudo cp /home/vagrant/.bashrc /root/ && sudo chown root:root /root/.bashrc
 
 echo ""
 echo "#########################"
--- databases/mysql.sh
+++ databases/mysql.sh
@@ -5,9 +5,9 @@ echo ""
 echo "#########################"
 echo "#   INSTALLING MYSQL    #"
 echo "#########################"
-sudo debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password password toor'
-sudo debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password_again password toor'
-sudo aptitude install -y mysql-server php5-mysql
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y mysql-server php5-mysql
 # mysql_secure_installation
 # sed -i "s/[mysqld](\n|\r\n)/[mysqld]\ncollation-server = utf8_general_ci\ncharacter-set-server = utf8\nskip-character-set-client-handshake" /etc/mysql/my.cnf
 # [mysqld]
